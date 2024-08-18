21a03ba079bbfd123403335cfea75bab48252a0a
Florian Barthel <florianbarthel@yahoo.de>
2024-08-18 16:19:19 +0200

    add sample scene

--- run_main.py
+++ run_main.py
@@ -3,7 +3,7 @@ from splatviz import Splatviz
 
 
 @click.command()
-@click.option("--data_path", help="root path for .ply files", metavar="PATH", default="./sample_scenes")
+@click.option("--data_path", help="root path for .ply files", metavar="PATH", default="./resources/sample_scenes")
 @click.option("--mode", help="[default, decoder, attach]", default="default")
 @click.option("--host", help="host address", default="127.0.0.1")
 @click.option("--port", help="port", default=6009)
